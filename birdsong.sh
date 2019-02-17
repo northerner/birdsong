b48e296450efc3930c8f2bc6257a5130c15060a1
Daniel King <daniel@Daniels-MacBook-Pro.local>
2019-02-17 09:28:07 +0000

    add paplay support

--- birdsong.sh
+++ birdsong.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-BIRD=`curl -L -I -s https://en.wikipedia.org/wiki/Special:RandomInCategory/Birds_of_Europe | grep 'location' | awk -F '/' '{ gsub("_", " "); print $NF }'`
+BIRD=`curl -L -I -s https://en.wikipedia.org/wiki/Special:RandomInCategory/Birds_of_Europe\
+  | grep 'location'\
+  | awk -F '/' '{ gsub("_", " "); print $NF }'`
 
 echo $BIRD
 
-URL=`curl -s https://www.xeno-canto.org/api/2/recordings?query=$BIRD | jq '.recordings[0].file' | awk '{ gsub("\"", ""); print substr($0,3) }'`
+URL=`curl -s https://www.xeno-canto.org/api/2/recordings?query=$BIRD | jq '.recordings[0].file'`
+
+URL=${URL//\"/}
+URL=${URL:2}
 
 echo $URL
-sleep .5
+sleep .2
 
 curl -L -s $URL --output birdaudio
 
-afplay birdaudio
+if [ -x "$(command -v paplay)" ]; then
+  paplay birdaudio
+elif [ -x "$(command -v afplay)" ]; then
+  afplay birdaudio
+fi
